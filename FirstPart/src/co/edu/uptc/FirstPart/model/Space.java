75190be7be7de935df3c327e9469f69d50b69f0a
Andres Maldonado <andres.maldonado02@uptc.edu.co>
2023-03-17 00:26:42 -0500

    Primer Ejercicio

--- FirstPart/src/co/edu/uptc/FirstPart/model/Space.java
+++ FirstPart/src/co/edu/uptc/FirstPart/model/Space.java
@@ -1,44 +1,30 @@
 package co.edu.uptc.FirstPart.model;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class Space {
 
-	public int eliminateSpace(String string) {
-		String path = "src/resources/input/texto.txt";
-		try {
+	private static String newText = "";
 
-			FileReader fileReader = new FileReader(path);
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			String line = bufferedReader.readLine();
-			FileWriter writer = new FileWriter("src/resources/NewFile.txt");
-
-			while (line != null) {
-
-				String lineWithoutSpaces = line.replaceAll("\\s+", "");
-				writer.write(lineWithoutSpaces);
-				writer.write("\n");
-
-				line = FileReader.read();
-			}
-
-			FileReader fileReader2 = new FileReader(null);
-			fileReader2.close();
-			writer.close();
-
-			System.out.println("Archivo procesado exitosamente.");
-
-		} catch (IOException e) {
-			e.printStackTrace();
+	public static String eliminateSpace(String path) throws IOException, FileNotFoundException {
+		FileReader fileReader = new FileReader(path);
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		String line;
+		while ((line = bufferedReader.readLine()) != null) {
+			newText += line.replaceAll("\\s+", "");
+			// newText += line.replace(" ", "");
 		}
+		bufferedReader.close();
+		return newText;
 	}
 
-	public static void main(String[] args) {
-		Space firstActivity = new Space();
-		System.out.println("Nueva frase" +eliminateSpace ("src/resources/input/newFile.txt"));
+	public static void writeNewFile(String path, String text) throws IOException, FileNotFoundException {
+		FileWriter writer = new FileWriter(path);
+		writer.write(text);
+		writer.close();
 	}
 }
-    
