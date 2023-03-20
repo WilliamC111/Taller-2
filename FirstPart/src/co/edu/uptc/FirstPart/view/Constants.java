323e86bf9eaa987cfb28bc416f76fccd8ca414ad
WilliamC111 <110690003+WilliamC111@users.noreply.github.com>
2023-03-19 22:02:40 -0500

    Delete FirstPart/src directory

--- FirstPart/src/co/edu/uptc/FirstPart/view/Constants.java
+++
@@ -1,6 +0,0 @@
-package co.edu.uptc.FirstPart.view;
-
-public class Constants {
-    public static final String PATH_INPUT = "Taller-2/FirstPart/src/resources/Phrase.txt";
-    public static final String PATH_OUTPUT = "Taller-2/FirstPart/src/resources/NewFile.txt";
-}
