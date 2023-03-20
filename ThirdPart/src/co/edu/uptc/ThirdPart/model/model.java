32f7e98e01d91c7053c54ee8a33f4a148cae2492
WilliamC111 <110690003+WilliamC111@users.noreply.github.com>
2023-03-19 22:10:10 -0500

    Update model.java

--- ThirdPart/src/co/edu/uptc/ThirdPart/model/model.java
+++ ThirdPart/src/co/edu/uptc/ThirdPart/model/model.java
@@ -1,5 +1,45 @@
 package co.edu.uptc.ThirdPart.model;
 
 public class model {
+  private static Vehicle readVehicle(String path) {
+		Vehicle vehicle  = new Vehicle();
+		try (DataInputStream stream = new DataInputStream(new FileInputStream(path))) {
+			vehicle.setBrand(stream.readUTF());
+		  vehicle.setModel(stream.readUTF());
+	    vehicle.setYear(stream.readInt());
+			vehicle.setLicensePlate(stream.readUTF());
+			vehicle.setColor(stream.readUTF());		
+		} catch (Exception e) {
+			System.err.println(e.getStackTrace());
+		}
+		return vehicle;
+	}
+		
+	public static int readId (String path) {
+	
+			int counter = 0;
+			try {
+				FileReader fileReader = new FileReader(path);
+				BufferedReader bufferedReader = new BufferedReader(fileReader);
+				String line = bufferedReader.readLine();
+				while (line != null) {
+					for (int i = 0; i < line.length(); i++) {
+							counter++;
+						}
+					}
+					line = bufferedReader.readLine();
+				
+				bufferedReader.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return counter;
+		}
+	private static int searchVehicle(int id) {
+		
+		return id = 0;
+	}
 
 }
